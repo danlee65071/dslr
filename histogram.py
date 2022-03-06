23b601fc4332992648853796fad252543c28564c
hcharlsi <hcharlsi@student.21-school.ru>
2022-03-06 10:43:40 +0300

    histogram

--- histogram.py
+++ histogram.py
@@ -1,6 +1,16 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-dt = pd.read_csv('datasets/dataset_test.csv')
+dt = pd.read_csv('datasets/dataset_train.csv')
+houses_names = dt['Hogwarts House'].unique()
+courses_name = dt.drop(['Index', 'Hogwarts House', 'First Name', 'Last Name', 'Birthday', 'Best Hand'], axis=1).columns
+for course in courses_name:
+    for house in houses_names:
+        x = dt[dt['Hogwarts House'] == house][course]
+        plt.hist(x, bins=20, label=houses_names, stacked=True, alpha=0.2)
+    plt.legend()
+    plt.title(course)
+    plt.xlabel('Mark')
+    plt.ylabel('Student count')
+    plt.show()
